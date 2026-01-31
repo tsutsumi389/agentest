116b17cffc86072f2efdd97cc1b0f839f495beda
tsutsumi389 <jtsutsumi1987@gmail.com>
2026-01-31 17:42:25 +0900

    feat: 不要なテストヘルパー関数を削除

--- apps/api/src/__tests__/integration/users.integration.test.ts
+++ apps/api/src/__tests__/integration/users.integration.test.ts
@@ -13,7 +13,6 @@ import {
   createTestCase,
   createTestCaseExpectedResult,
   createTestEnvironment,
-  createTestExecution,
   createTestExecutionTestSuite,
   createTestExecutionTestCase,
   createTestExecutionTestCaseExpectedResult,
